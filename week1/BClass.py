8e9f6f6e22209355d67a4faebf6c0f7ec37db2af
Nora <nora.wernerson@gmail.com>
2017-11-18 15:05:57 +0100

    new class with a,c,d,r,B

---
+++ week1/BClass.py
@@ -0,0 +1,32 @@
+
+class Bclass(object):
+    """docstring for Bclass."""
+    a_i = 0
+    c_i = 0
+    d_i = 0
+    r_i = 0
+    B_i = 0
+    def __init__(self, arg):
+    super(Bclass, self).__init__()
+    self.arg = arg
+
+def setA(value):
+    a_i = value
+def getA():
+    return a_i
+def setC(value):
+    c_i = value
+def getC():
+    return c_i
+def setD(value):
+    d_i = value
+def getD():
+    return d_i
+def setR(value):
+    r_i = value
+def getR():
+    return r_i
+def setB(value):
+    b_i = value
+def getB():
+    return b_i
