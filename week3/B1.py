b728348d1e0194a1f53d7b269123b45f91319805
Nora <nora.wernerson@gmail.com>
2017-11-29 11:52:47 +0100

    Here wo go again

--- week3/B1.py
+++ week3/B1.py
@@ -5,6 +5,7 @@ import hashlib
 def commitment(v, nbrOfRandBits): #v is a one bit value
      k = random.getrandbits(nbrOfRandBits)
      x = hashSha1(v, k) #how to hash with to inputs
+     return x
 
 # we want to hash with two input parameters: the value and some randomness
 
@@ -16,3 +17,8 @@ def hashSha1(bytein, randomIn):
 
 
 def simulation(V, nbrOfSimulations):
+
+b'\0'
+b'\x00\x00\x0bQ'
+
+print(commitment())
