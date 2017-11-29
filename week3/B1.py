2e58e20243ef8fa78d7c69867827ba48cfe5843b
Nora <nora.wernerson@gmail.com>
2017-11-29 11:51:01 +0100

    laggt till dubla input i hashfunktionen

--- week3/B1.py
+++ week3/B1.py
@@ -1,16 +1,18 @@
 #commitment scheme
 import random
+import hashlib
 
 def commitment(v, nbrOfRandBits): #v is a one bit value
      k = random.getrandbits(nbrOfRandBits)
-     x = hashSha1(v) #how to hash with to inputs
-
-
-
+     x = hashSha1(v, k) #how to hash with to inputs
 
 # we want to hash with two input parameters: the value and some randomness
 
-def hashSha1(bytein):
+def hashSha1(bytein, randomIn):
     sha1 = hashlib.sha1()
     sha1.update(bytein)
+    sha1.update(randomIn)
     return sha1.digest()
+
+
+def simulation(V, nbrOfSimulations):
