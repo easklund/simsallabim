0bea2592a1911f8ce119ec594a17d4cb0092de54
Nora <nora.wernerson@gmail.com>
2017-11-29 12:08:20 +0100

    testar commitment och check metoderna

--- week3/B1.py
+++ week3/B1.py
@@ -2,12 +2,21 @@
 import random
 import hashlib
 
-def commitment(v, nbrOfRandBits): #v is a one bit value
-     k = random.getrandbits(nbrOfRandBits)
-     x = hashSha1(v, k) #how to hash with to inputs
+def commitment(v, k): #v is a one bit value
+     #k = random.getrandbits(nbrOfRandBits)
+     x = hashSha1(v, k) #how to hash with to input
      return x
 
 # we want to hash with two input parameters: the value and some randomness
+
+def check(x, v, k):
+    x2 = hashSha1(v, k)
+    print(x2)
+    if x == x2 :
+        return "true"
+    else :
+        return "false"
+
 
 def hashSha1(bytein, randomIn):
     sha1 = hashlib.sha1()
@@ -17,8 +26,10 @@ def hashSha1(bytein, randomIn):
 
 
 def simulation(V, nbrOfSimulations):
+    pass
+#v = b'\0'
+#k = b'\x00\x00\x0bQ'
 
-b'\0'
-b'\x00\x00\x0bQ'
+print(commitment(b'\0', b'\x00\x00\x0bQ'))
 
-print(commitment())
+print(check(commitment(b'\0', b'\x00\x00\x0bQ'), b'\1', b'\x00\x00\x0bQ'))
