a33eaddb8c3a7cc060a5a9da61ad2bf72527b458
easklund <dat14eas@student.lu.se>
2017-12-05 11:45:23 +0100

    rensat B2

--- week3/B2.py
+++ week3/B2.py
@@ -2,12 +2,9 @@
 #create a master secret using Lagrange
 #f(z) = x + α1z + α2z^2 +. . . + αt−1z^t−1
 
-#f(0)= -26210
-
 def ceratePrivatePoly():
     pass
 
-# f1(x) = 13 + 8x + 11x^2 + 1x^3 + 5x^4
 def countF1of1(poly):
     size = len(poly)
     summa = 0
@@ -24,9 +21,6 @@ def Fof1(poly, f1):
     return F1
 
 def revealMasterSecret(listOfKPoly):
-    #interpolate
-    #secret = sumOF(f(i)) * multiplicationssummaOF(j(j-i))
-    #sumOf = 0
     secret = 0
     size = len(listOfKPoly)
     for i in range(size):
@@ -36,7 +30,7 @@ def revealMasterSecret(listOfKPoly):
             if j != i and listOfKPoly[j] != 0 :
                 mulOf = mulOf * (j+1)/((j+1)-(i+1))
         secret += (f * mulOf)
-    return int(secret)
+    return round(secret)
 
 def addF1InPoly(f1, poly):
     p = []
@@ -45,17 +39,14 @@ def addF1InPoly(f1, poly):
         p.append(poly[i])
     return p
 
-privatepoly = [ 13, 8, 11, 1, 5]
-fOf1 =  [ 75, 75, 54, 52, 77, 54, 43]
-collaberationPoly = [2782, 0, 30822, 70960, 0, 256422]
+privatepoly = [ 4, 4, 7, 9]
+fOf1 =  [ 34, 52, 36, 34, 35, 39]
+collaberationPoly = [0, 2080, 0, 0, 12469, 19052]
 
 f1Of1 = countF1of1(privatepoly)
 fOf1 = Fof1(fOf1, f1Of1)
 p = addF1InPoly(fOf1, collaberationPoly)
 secret = revealMasterSecret(p)
 
-
-print(f1Of1)
-print(fOf1)
 print(p)
 print(secret)
