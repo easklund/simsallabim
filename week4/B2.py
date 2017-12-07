dbbc77457d2931d68b9aa9b3d05e634db5e014cf
easklund <dat14eas@student.lu.se>
2017-12-07 17:31:16 +0100

    B2 börjar ta form igen

--- week4/B2.py
+++ week4/B2.py
@@ -12,12 +12,8 @@ def genValidSign(name, grade):
         for i in range(16):
             value= intToHex(i)
             payload={'grade': grade, 'name' : name, 'signature': value}
-
-            #time = float(timeit.Timer('requests.get("https://eitn41.eit.lth.se:3119/ha4/addgrade.php?", params=payload)')
             r = requests.get('https://eitn41.eit.lth.se:3119/ha4/addgrade.php', params=payload, verify = False)
             time = r.elapsed.total_seconds()
-            # if bestTime == None:
-            #     bestTime = time
             if time >bestTime:
                 bestTime = time
                 # print("i: ", i)
@@ -41,11 +37,6 @@ def ListToString(list):
     for i in range(size):
         string = string + list[i]
     return string
-
-    # python requeste
-    # s = HMAC(name||grade, k)
-#return a valid signature
-#name=Kalle&grade=5&signature=1
 
 def intToHex(i):
     n = format(i,'01x')
