a59108e5ab9c70b05890e0c1f2f568a506104736
easklund <dat14eas@student.lu.se>
2017-12-08 12:47:38 +0100

    B2 beskrivning på del 1

--- week4/B3.py
+++ week4/B3.py
@@ -1,6 +1,16 @@
 
 
 def mgf1(mgfSeed, maskLen):
+    hLen = 2**32
+    #If maskLen > 2^32 hLen, output "mask too long" and stop.
+    if len(maskLen) > hLen :
+        print("mask no long")
+        return -1
+    #Let T be the empty octet string.
     T = ''
-    for i in range((maskLen / hLen) - 1):
-        
+    #For counter from 0 to \ceil (maskLen / hLen) - 1, do the following:
+    for i in range((len(maskLen) / hLen) - 1):
+        #
+
+    #Output the leading maskLen octets of T as the octet string mask.
+    return leadingMaskLen
