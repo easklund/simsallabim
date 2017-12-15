210975b5a4062635b827bb8bc8dd6b39d213de53
Nora <nora.wernerson@gmail.com>
2017-12-15 12:07:52 +0100

    Added some files and started on B1, but it is not working yet

--- week5/B1.py
+++ week5/B1.py
@@ -1,10 +1,17 @@
 #RSA key
-
+from Crypto.PublicKey import RSA
 
 def crackDecryption(M, EM):
+    return RSA.decrypt(M,EM)
 
 
-    pass
 
 
-def recoverKey(key): #key is in PEM format
+def recoverKey(): #key is in PEM format
+    key = open('key.pem', 'r') # do I need to decrypt the key first?
+    key = RSA.importKey(key.read())
+    return key
+
+
+#print(recoverKey())
+crackDecryption('This is secret','Qe7+h9OPQ7PN9CmF0ZOmD32fwpJotrUL67zxdRvhBn2U3fDtoz4iUGRXNOxwUXdJ2Cmz7zjS0DE8ST5dozBysByz/u1H//iAN+QeGlFVaS1Ee5a/TZilrTCbGPWxfNY4vRXHP6CB82QxhMjQ7/x90/+JLrhdA99lvmdNetGZjY=')
