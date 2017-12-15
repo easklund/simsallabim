dea711ceb4a1e45f240a8484e783f6f628beb610
Nora <nora.wernerson@gmail.com>
2017-12-15 13:05:21 +0100

    B1 is working properly

--- week5/B1.py
+++ week5/B1.py
@@ -1,17 +1,28 @@
 #RSA key
 from Crypto.PublicKey import RSA
-
-def crackDecryption(M, EM):
-    return RSA.decrypt(M,EM)
-
-
-
+from Crypto import Random
+from Crypto.Cipher import PKCS1_v1_5
+from Crypto.Hash import SHA
+from base64 import b64decode
 
 def recoverKey(): #key is in PEM format
-    key = open('key.pem', 'r') # do I need to decrypt the key first?
+    key = open('key.pem', 'rb')
     key = RSA.importKey(key.read())
-    return key
+    e = getattr(key, 'e')
+    p = getattr(key, 'p')
+    q = getattr(key, 'q')
+    d = getattr(key, 'd')
+    n = p * q
+    return RSA.construct((n,e,d,p,q))
+
+def decryptM(EM):
+    decoded_data = b64decode(EM) #Because it is = in the end of the encrypted message we know it is Base64
+    dsize = SHA.digest_size
+    key = recoverKey()
+    s = Random.new().read(15 + dsize)
+    cipher = PKCS1_v1_5.new(key)
+    m = cipher.decrypt(decoded_data,s)
+    return m
 
 
-#print(recoverKey())
-crackDecryption('This is secret','Qe7+h9OPQ7PN9CmF0ZOmD32fwpJotrUL67zxdRvhBn2U3fDtoz4iUGRXNOxwUXdJ2Cmz7zjS0DE8ST5dozBysByz/u1H//iAN+QeGlFVaS1Ee5a/TZilrTCbGPWxfNY4vRXHP6CB82QxhMjQ7/x90/+JLrhdA99lvmdNetGZjY=')
+print('Message: ', decryptM('T9FAfFVcVCdPH45kv3OU/Kot9NOyQ2t5tWI1GW6nJ4Ul435T68wq1f1vm3KhDcKONzdN3krJ/VwlIzdssIcqmVizw5mnMupmd1gNmf7EKLZWjT4LaMQhDMijrfhxCdbiQKjKqYnUehlOCeDS0JXOJpiYcCtbmTVYHBmxBuOZ1l8='))
