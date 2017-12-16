ceade586ade8ed6a415ecbc1bc8cd9ae706617da
easklund <dat14eas@student.lu.se>
2017-12-16 19:36:11 +0100

    B3 shirt borde vara klar

--- week5/B3.py
+++ week5/B3.py
@@ -1,4 +1,5 @@
 from base64 import b64decode
+import binascii
 
 
 def convertToDER(integer):
@@ -9,12 +10,41 @@ def convertToDER(integer):
     #lägg till value
     pass
 
-def convertShort():
-    pass
+def convertShort(i, length):
+    # TLV where type is integer that has tag 2
+    typeInt = hexToByte('02')
+    l = hexToByte(length)
+    value = intToByte(i)
+    return byteToHex(typeInt + l + value)
 
-def convertLong():
-    pass
+def convertLong(value, length):
+    # TLV where type is integer that has tag 2
+    typeInt = hexToByte('02')
+    size = len(length)
+    #Kolla hur många octets vi ska ha
+    #1 sen hur många octets som följer som repreensterar längden
+    #restern va octeterna som respresenterar längden
+    l = hexToByte(length)
+    value = intToByte(i)
+    return byteToHex(typeInt + l + value)
 
 def DERToBase64(der):
     base64form = b64encode(der)
     return base64form
+
+def hexToByte(hexa):
+    d = binascii.unhexlify(hexa)
+    return d
+
+def byteToHex(byte):
+    hexa = binascii.hexlify(byte).decode('utf-8')
+    return hexa
+
+def intToByte(integer):
+    size = (integer.bit_length() + 7 ) // 8
+    if integer == 0:
+        size = 1
+    four_bytes = integer.to_bytes(size, byteorder='big')
+    return four_bytes
+
+print(convertShort(1234, '12'))
